6b436e4abbaad009f20783a7b8288e4e0f0c15e6
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-27 11:43:32 +0200

    add header tag to our site

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,11 @@ function App() {
   return (
     <div className="App">
       <div className="container-lg px-sm-5">
-        <Heading/>
+        <Heading />
+        <header className="py-3 px-4 text-center">
+          <h1 className="fHL">Design solutions made easy</h1>
+          <p className="small textGray px-5 px-md-4">With over ten years of experience in various design disciplines, I’m your one-stop shop for your design needs.</p>
+        </header>
       </div>
     </div>
   );
