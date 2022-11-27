5d7caed6a826b1c9f79e6b85f5a44618054bd722
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-27 11:30:32 +0200

    implement header Component

---
+++ src/Components/Header/Heading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className=' py-3 d-flex justify-content-between align-items-center'>
+          <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fill-rule="evenodd">
+                  <path fill="#030303" d="M0 0h64v64H0z" />
+                  <circle stroke="#FFF7F0" stroke-width="4" cx="25" cy="25" r="11" />
+              </g>
+          </svg>
+        <button className='mainBtn'>Free Consultation</button>
+    </div>
+  )
+}
