71cac6bad5f2d66dc87e00febd74a832ad91bb86
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-27 16:16:28 +0200

    change svg attribute which is towards into camelCase

--- src/Components/Header/Heading.jsx
+++ src/Components/Header/Heading.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-export default function () {
+export default function Heading() {
   return (
+    <div className="container-lg px-sm-5">
     <div className=' py-3 d-flex justify-content-between align-items-center'>
           <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
-              <g fill="none" fill-rule="evenodd">
+              <g fill="none" fillRule="evenodd">
                   <path fill="#030303" d="M0 0h64v64H0z" />
-                  <circle stroke="#FFF7F0" stroke-width="4" cx="25" cy="25" r="11" />
+                  <circle stroke="#FFF7F0" strokeWidth="4" cx="25" cy="25" r="11" />
               </g>
           </svg>
         <button className='mainBtn'>Free Consultation</button>
+    </div>
     </div>
   )
 }
