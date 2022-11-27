fbce821cbd39948677c77b396917dc104b2d579b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-27 19:46:56 +0200

    update component layout spacing

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -4,7 +4,7 @@ import Image from '../../assets/image-amy.webp'
 export default function Info() {
   return (
     <div className="container-lg px-sm-5">
-    <div className='row gy-4 gx-5 py-5 my-5 align-items-center' >
+    <div className='row gy-4 gx-5 py-5 my-3 align-items-center' >
        <div className='col-md-6 col-xl-5 col-10  mx-auto'>
           <div>
              <img src={Image} alt="userImage" className='w-100'/>
