d244773c098df29f08482246460ba270b8bb1ba7
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-27 20:14:21 +0200

    adjust layout

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
         <Heading />
-        <div className="container-lg px-sm-5">
-        <header className="py-3 px-4 text-center">
+        <div className="container-lg px-md-5">
+        <header className="py-3 px-md-4 px-2 text-center">
           <h1 className="fHL">Design solutions made easy</h1>
-          <p className="small textGray px-5 px-md-4 headWidth mx-auto">With over ten years of experience in various design disciplines, I’m your one-stop shop for your design needs.</p>
+          <p className="small textGray px-sm-5 px-2 px-md-4 headWidth mx-auto">With over ten years of experience in various design disciplines, I’m your one-stop shop for your design needs.</p>
         </header>
         </div>
         <Skills />
